4e05425af6b59da781124845010be56b3568e025
KayaLuken <kayaluken@gmail.com>
2025-07-12 13:54:40 +0200

    feat: proxy api to backend url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/crawl',
+        destination: `${process.env.BACKEND_URL || 'http://localhost:8080'}/crawl`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
